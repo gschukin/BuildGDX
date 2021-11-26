b146aeaa566268e23822df2c050128c8cddc4abc
m210 <m210-2007@mail.ru>
2021-11-26 16:54:02 +0300

    Version

--- core/src/ru/m210projects/Build/Engine.java
+++ core/src/ru/m210projects/Build/Engine.java
@@ -135,7 +135,7 @@ public abstract class Engine {
 	 *  	bithandler
 	 */
 
-	public static final String version = "21.110"; // XX. - year, XX - month, X - build
+	public static final String version = "21.111"; // XX. - year, XX - month, X - build
 
 	public static final byte CEIL = 0;
 	public static final byte FLOOR = 1;
