cc82baacc8249e3fe3634c2bf11730e62f072e26
m210 <m210-2007@mail.ru>
2023-06-05 18:53:48 +0300

    refactoring2

---
+++ core/src/ru/m210projects/Build/osd/ConsoleLogger.java
@@ -0,0 +1,61 @@
+package ru.m210projects.Build.osd;
+
+import ru.m210projects.Build.FileHandle.Compat;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static ru.m210projects.Build.Strhandler.toLowerCase;
+
+public class ConsoleLogger {
+
+    private final OutputStream out;
+    private final Path path;
+    private boolean closed;
+
+    public ConsoleLogger(String filename) throws IOException {
+        this.path = Paths.get(Compat.Path.User.getPath() + toLowerCase(filename));
+        this.out = new BufferedOutputStream(Files.newOutputStream(path));
+        this.closed = false;
+    }
+
+    public void write(String message) {
+        if(closed) {
+            return;
+        }
+
+        try {
+            out.write(String.format("%s\r\n", message).getBytes());
+        } catch (IOException ignored) {
+        }
+    }
+
+    @Override
+    public String toString() {
+        try(InputStream is = Files.newInputStream(path)) {
+            byte[] data = new byte[is.available()];
+            int len = is.read(data);
+            if (len > 0) {
+                return new String(data);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "Can't read the log file";
+    }
+
+    public void close() {
+        if(closed) {
+            return;
+        }
+
+        try {
+            out.close();
+            closed = true;
+        } catch (Exception ignored) {
+        }
+    }
+
+}
---
+++ core/src/ru/m210projects/Build/osd/commands/OsdCallback.java
@@ -0,0 +1,22 @@
+package ru.m210projects.Build.osd.commands;
+
+import org.jetbrains.annotations.NotNull;
+import ru.m210projects.Build.osd.CommandResponse;
+
+public class OsdCallback extends OsdCommand {
+
+    private final OsdRunnable callback;
+
+    public OsdCallback(@NotNull String name, @NotNull String description, OsdRunnable callback) {
+        super(name, description);
+        this.callback = callback;
+    }
+
+    public CommandResponse execute(String[] argv) {
+        return callback.execute(argv);
+    }
+
+    public interface OsdRunnable {
+        CommandResponse execute(String[] argv);
+    }
+}
